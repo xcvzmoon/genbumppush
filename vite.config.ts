4e8fb26b26ef6852b6366de5a2513b303f5bfc01
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-09-12 22:52:18 +0800

    chore(test): enable verbose vitest reporters

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     include: ['tests/**'],
     passWithNoTests: true,
     testTimeout: 15_000,
+    reporters: 'verbose',
   },
   pack: {
     entry: ['src/index.ts', 'src/bin.ts'],
